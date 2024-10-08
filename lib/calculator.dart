cacadf9df339e57cc6a74924cb6e0380b34959b4
gabriel1003 <gabriel.samersla97@gmail.com>
2024-10-08 16:24:31 -0300

    projeto pronto para teste

--- lib/calculator.dart
+++ lib/calculator.dart
@@ -60,6 +60,91 @@ _CalculatorState createState() => _CalculatorState();
 
   Widget _buildKeyboardButton(String label,
 {int flex = 1, Color textColor = Colors.white, Color backgroundColor = Colors.black}) {
-  return Expanded(child: );
+  return Expanded(
+    flex: flex,
+    child: RaisedButton(
+      color:  backgroundColor,
+      textColor: textColor,
+      child: Text(
+        label,
+        style: TextStyle(fontSize: 24),
+      ),
+      onPressed: () {
+        setState(() {
+          _memory.applyCommand(label);
+        });
+      },
+    ),
+    );
+}
+
+Widget _buildKeyboard() {
+  return Container(
+    color:  Colors.black,
+    height: 400.0,
+    child: Column(
+      children: <Widget>[
+        Expanded(
+          flex: 1,
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              _buildKeyboardButton('AC', textColor: Colors.deepOrange),
+              _buildKeyboardButton('DEL', textColor: Colors.deepOrange),
+              _buildKeyboardButton('%', textColor: Colors.deepOrange),
+              _buildKeyboardButton('÷', textColor: Colors.deepOrange),
+            ],
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              _buildKeyboardButton('7'),
+              _buildKeyboardButton('8'),
+              _buildKeyboardButton('9'),
+              _buildKeyboardButton('x', textColor: Colors.deepOrange),
+            ],
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              _buildKeyboardButton('4'),
+              _buildKeyboardButton('5'),
+              _buildKeyboardButton('6'),
+              _buildKeyboardButton('+', textColor: Colors.deepOrange),
+            ],
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              _buildKeyboardButton('1'),
+              _buildKeyboardButton('2'),
+              _buildKeyboardButton('3'),
+              _buildKeyboardButton('-', textColor: Colors.deepOrange),
+            ],
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              _buildKeyboardButton('0', flex: 2),
+              _buildKeyboardButton('.'),
+              _buildKeyboardButton('=', textColor: Colors.deepOrange),
+            ],
+          ),
+        ),
+      ],
+    ),
+  );
 }
   }
